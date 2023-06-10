18a886b772f3ab25350c8ab7e9e4232847977486
20203TN058@utez.edu.mx <Nahum Torres>
2023-06-09 20:34:00 -0600

    Limpieza de datos

---
+++ Clase de limpieza de datos.R
@@ -0,0 +1,59 @@
+#Clase del 08/06/2023
+
+#Leer archivos
+setwd("C:\\Users\\CC7\\Downloads")
+contaminacion <- read.csv("contaminacion.csv")
+
+#Crea una tabla de los datos, solo es visual, mejora  
+View(contaminacion)
+is.na(contaminacion)
+complete.cases(contaminacion)#Un valor por cada registro
+na.omit(contaminacion)#Omite las filas que tienen valores vacíos
+
+#Complete cases, negado nos devuleve si dentro de las filas hay celdas vacias 
+contaminacion[!complete.cases(contaminacion),]
+
+install.packages(mice)
+library(mice)
+
+maraton <- read.csv("maraton_ny.csv")
+
+md.pattern(maraton)
+md.pattern(contaminacion)
+
+duplicated(contaminacion)
+
+contaminacion[duplicated(contaminacion),]
+
+#Técnicas para la limpieza de datos
+#Se le conoce como data imputation -> que es llenarlos 
+#Al proceso reemplazar los datos incompletos o valores faltantes
+#Para ello exiten distintas ´tecnicas, entre las que se encuentran: 
+#Promedio
+#Moda
+#Vecinos cercaos -> Los valores más acercados al registro
+
+sum(contaminacion$CN) #-> Si sumas valores enteros con vacíos, obviamente te van a dar null
+
+sum(contaminacion$CN, na.rm = TRUE)# na.rm remueve a los vacíos
+
+contaminacion$CN_p <- ifelse(is.na(contaminacion$CN),#Busca los vacíos
+                             mean(contaminacion$CN, na.rm = TRUE),#LOs rellena con el promedio
+                             contaminacion$CN)#Los valores que no estan vacíos les pone el mismo valor
+
+contaminacion
+
+install.packages("modeest")
+library("modeest")
+
+mfv(contaminacion)
+
+#Promedio cuando haya datos numericos y moda con text
+b <- data.frame(v1 = c("a","b","c","d"), v2 = c("hola","hola",NA,"adios"))
+b$v2_moda <- ifelse(is.na(b$v2), mfv(b$v2, na_rm=TRUE), b$v2)
+b
+
+#Vecinos cercanos
+#K-nearet Neighbors (KNN)
+#método de clasificación supervisado que calcular la dsitancia entre los elementos
+#a clasificar y el resto de los elementos en el dataset de entrenamiento
--- Ejercicio de practica.R
+++ Ejercicio de practica.R
@@ -41,3 +41,39 @@ nlevels(tipos)
 #Obtener la pizza mas cara
 pizzas[which.max(pizzas$price), 5]
 
+#Cual es el tamaño de la pizzas que se vende menos
+install.packages("dplyr")
+library("dplyr")
+
+#Obtener el conteo de todos los tamaños de las pizzas
+pizzas%>%count(size) #Devuelve un filas y columnas
+#Arrange es para ordernar los datos, se usa n por las columnas del anterior código
+#[filas, columnas]
+(pizzas%>%count(size) %>% arrange(n))[1,"size"]
+#Esto indica  %>% que va a mandar el resultado de los resultados anteriores al que sigue
+
+#Mostrar las pizzas vendidas de marzo a abrirl solo pizzan grandes
+#Con dplyr
+View(pizzas %>% filter(size == "L"
+                       & as.Date(date) >= '2015-03-01'
+                       & as.Date(date) <= '2015-04-30'))
+
+#Sin dplyr
+pizzas[pizzas$size == "L" & substr(pizzas$date,6,7) == "03"
+       | substr(pizzas$date,6,7) == "04",]
+
+#Ventas realizadas en agosto
+resultado <- pizzas %>% filter(as.Date(date) >= '2015-08-01' & 
+                  as.Date(date) <= '2015-08-31') %>% select (name, size)
+View(resultado)
+# yA AMONOS
+
+#sin dplyr
+pizzas[pizzas$date >= '2015-08-01' & pizzas$date <='2015-08-31', c(5,6)]
+
+pizzas[substr(pizzas$date,6,7) == "08", c(5,6) ]
+
+#Cual es el horarario de atención de la pizzeria
+minimo <- min(pizzas$time)
+maximo <- max(pizzas$time)
+print(paste(minimo, "-",maximo))
